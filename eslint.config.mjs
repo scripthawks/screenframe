23e627e07cd6dc799460bb840c171badd48f5dba
eugenkin <evgensbk9@gmail.com>
2025-10-19 15:13:43 +0700

    settings linters

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,40 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint, { parser } from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import { defineConfig } from "eslint/config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default defineConfig([
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    parser: tseslint / parser,
+    parserOptions: {
+      project: "./tsconfig.json",
+      ecmaFeatures: { "jsx": true },
+      ecmaVersion: latest
+    },
+    plugins: {
+      js,
+      prettier: prettierPlugin,
+      tseslint
+    },
+    extends: [
+      "js/recommended",
+      "next/core-web-vatals",
+      prettierConfig
     ],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      ...prettierPlugin.configs.recommended.rules,
+      'no-console': 'warn',
+      'eqeqeq': 'warn',
+      'curly': 'warn',
+      'no-else-return': 'warn'
+    },
+    ignorePatterns: ["**/node_modules/*", "dist/*", "build", "*.html", "*.css"]
   },
-];
-
-export default eslintConfig;
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
