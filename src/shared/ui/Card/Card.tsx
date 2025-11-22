0f48b040ada917cffe6708cd9aad7a6d76016656
ula28 <ula24499@gmail.com>
2025-11-22 19:18:04 +0100

    feat: create Card component with Storybook stories (QQ-37)

---
+++ src/shared/ui/Card/Card.tsx
@@ -0,0 +1,24 @@
+import {ReactNode} from "react";
+import clsx from "clsx";
+import styles from './Card.module.scss'
+
+export interface CardProps {
+    title?: ReactNode
+    children: ReactNode
+    footer?: ReactNode
+    className?: string
+}
+
+export const Card: React.FC<CardProps> = ({ title, children, footer, className }) => {
+    return (
+        <div className={styles.cardWrapper}>
+            <div className={clsx(styles.cardField, className)}>
+                {title && <div className={styles.cardHeader}>{title}</div>}
+                <div className={styles.cardContent}>{children}</div>
+                {footer && <div className={styles.cardFooter}>{footer}</div>}
+            </div>
+        </div>
+    )
+}
+
+Card.displayName = 'Card'
