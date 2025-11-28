cc4a13fa93229d70592395bb255a69420d3c1be2
eugenkin <evgensbk9@gmail.com>
2025-11-29 00:54:13 +0700

    QQ-37:feat: create Header component

---
+++ src/widgets/header/Header.tsx
@@ -0,0 +1,49 @@
+'use client'
+import React, { useState } from 'react'
+import s from './Header.module.scss'
+import { Button, SelectBox, Typography } from '@/shared/ui'
+import { BellOutlineIcon, FlagRussia, FlagUnitedKingdom } from '@/shared/assets/icons'
+import Link from 'next/link'
+
+const options = [
+  { value: 'Russian', icon: <FlagRussia /> },
+  { value: 'UK', icon: <FlagUnitedKingdom /> },
+]
+
+export const Header = ({ is_auth = false }: { is_auth?: boolean }) => {
+  const [lang, setLang] = useState(options[0].value)
+
+  return (
+    <div className={s.wrapper}>
+      <div className={s.container}>
+        <Typography variant="large">Inctagram</Typography>
+        <div className={s.content}>
+          {is_auth && (
+            <Button variant="withIcon" className={s.bellButton}>
+              <BellOutlineIcon />
+              <Typography as="span" variant="small">
+                3
+              </Typography>
+            </Button>
+          )}
+
+          <SelectBox
+            options={options}
+            className={s.select}
+            value={lang}
+            onChange={value => setLang(value)}
+          />
+
+          {!is_auth && (
+            <div className={s.buttonsContainer}>
+              <Button variant="text">Log in</Button>
+              <Button asChild>
+                <Link href="/sign-up">Sign up</Link>
+              </Button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
